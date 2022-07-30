6c6d47bb75283ce65b0208362111e27bcb617a10
Mohammad Hadi <mhn.hadi@outlook.com>
2022-07-30 18:44:13 +0430

    urls bug fixed

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
-@app.route('all-process')
+@app.route('/all-process')
 def all_process():
     return os.popen('wmic process get description, processid').read()
 
-@app.route('kill')
+@app.route('/kill')
 def kill_process():
     process_name = request.args.get('processName')
     return os.popen('taskkill /IM ' + process_name + ' /F')
