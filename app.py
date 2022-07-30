fe75051ff427ebb4b37c672483c2ef981c242cee
Mohammad Hadi <mhn.hadi@outlook.com>
2022-07-30 18:42:48 +0430

    names changed

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from flask import Flask, request
 import os
 
+app = Flask(__name__)
 
-output = os.popen('wmic process get description, processid').read()
+@app.route('all-process')
+def all_process():
+    return os.popen('wmic process get description, processid').read()
 
-
-print(output)
+@app.route('kill')
+def kill_process():
+    process_name = request.args.get('processName')
+    return os.popen('taskkill /IM ' + process_name + ' /F')
